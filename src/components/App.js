a396f090e787f60dc7f7c814a4f5b29fa87ca028
PranayRathi <40464551+PranayRathi@users.noreply.github.com>
2020-10-21 21:31:47 +0530

    change1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  function onChange() {
+    return (
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy.
+      </p>
+    );
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onChange={() => onChange()}>
+        click
+      </button>
     </div>
   );
 }
-
 
 export default App;
