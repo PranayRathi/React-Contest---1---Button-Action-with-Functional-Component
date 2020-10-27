70c8adc7d4279d1d6ce2edc9b3a2e68d5a755d8a
PranayRathi <40464551+PranayRathi@users.noreply.github.com>
2020-10-28 02:10:16 +0530

    try6

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,22 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  function onChange() {
-    return (
-      <p id="para">
-        Hello, I've learnt to use the full-stack evaluation tool. This makes me
-        so happy.
-      </p>
-    );
+  const [paraState, setParaState] = React.useState(false);
+
+  function handleClick() {
+    setParaState(true);
   }
   return (
     <div id="main">
-      <button id="click" onChange={() => onChange()}>
+      <button id="click" onClick={handleClick}>
         click
       </button>
+      {paraState && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </div>
   );
 }
