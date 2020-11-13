1860ff0b7782e9d0c7b49ae999e1a5474a09ef83
PranayRathi <40464551+PranayRathi@users.noreply.github.com>
2020-11-13 22:43:17 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,25 @@
-import React, { Component, useState } from "react";
-import "./../styles/App.css";
+import React from "react";
+//import "./../styles/App.css";
 
 function App() {
-  const [display, setDisplay] = React.useState(false);
+  const [visible, setVisible] = React.useState(false);
 
-  function handleClick() {
-    setDisplay(true);
-  }
   return (
     <div id="main">
-      <button id="click" onClick={handleClick}>
+      <button
+        id="click"
+        onClick={() => {
+          setVisible(true);
+        }}
+      >
         click
       </button>
-      {display && (
+      {visible ? (
         <p id="para">
           Hello, I've learnt to use the full-stack evaluation tool. This makes
           me so happy
         </p>
-      )}
+      ) : null}
     </div>
   );
 }
